41ef8101b360c75a3eb8317c92afebdfe3b9e067
Tin Po CHAN <ericchantinpo@hotmail.com>
2025-12-13 21:31:22 +0800

    Seo json ld

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.allout.hk", // ⚠️ 之後記得換成你正式 domain
+  integrations: [sitemap()],
+});
--- src/content/blog/first-post.md
+++ src/content/blog/first-post.md
@@ -8,3 +8,9 @@ coverImage: "https://example.com/blog/quotation-cover.jpg"
 ---
 
 # 裝修報價單應該點睇？
+
+### 常見報價陷阱一：項目描述過於籠統
+
+![報價單項目寫得含糊的例子](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUfBu7QIxGiCXy3f-JC1P3KO1JVvYeMMsdmg&s)
+
+如果你見到「清拆工程（全包）」而冇細項，風險就好高。
